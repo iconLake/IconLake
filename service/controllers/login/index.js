8fff0925e516cf25f404dc8ef6b1d161ec1f8fc5
幻想 <huanxiangspace@qq.com>
2022-06-30 23:18:56 +0800

    获取登录参数

--- service/controllers/login/index.js
+++ service/controllers/login/index.js
@@ -1,7 +1,13 @@
 import { User } from '../../models/user.js'
 import { getLocale, setLocale } from '../../utils/index.js'
 import { ROOT, RESOURCE_MAX_AGE } from '../../utils/const.js'
+import { getConfig } from '../../config/index.js'
 
+const config = getConfig()
+
+/**
+ * 登录页
+ */
 export default async function index (req, res) {
   setLocale(req, res)
   if (req.cookies.token) {
@@ -19,5 +25,17 @@ export default async function index (req, res) {
   res.sendFile(`./public/login/index.${getLocale(req)}.html`, {
     root: ROOT,
     maxAge: RESOURCE_MAX_AGE
+  })
+}
+
+/**
+ * @api {get} /login/params
+ */
+export async function params (req, res) {
+  res.json({
+    clientId: {
+      gitee: config.gitee.clientId,
+      github: config.github.clientId
+    }
   })
 }
