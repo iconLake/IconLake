6aacadfff5e99fb826a1ce75ebd6bcf02242242e
幻想 <huanxiangspace@qq.com>
2022-08-01 15:59:04 +0800

    build www

---
+++ service/public/manage/assets/Invite.c255f152.js
@@ -0,0 +1,1 @@
+import{d as c,C as r,u as n}from"./vendor.75eb7f42.js";import{t as u,x as p}from"./project.90d58fa9.js";import"./index.f69bfaca.js";const j=c({setup(i){const o=r(),t=n(),e=o.params.id,s=o.query.code;async function a(){await p(e,s),t.replace(`/icons/${e}`)}return e&&s?a():(u("\u53C2\u6570\u9519\u8BEF"),t.replace("/home")),()=>{}}});export{j as default};
