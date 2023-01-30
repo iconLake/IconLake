0b9ac904e44b673e9bfc5334537193dc04dbb612
幻想 <huanxiangspace@qq.com>
2023-01-30 23:53:30 +0800

    修复options

--- extension/src/options/App.vue
+++ extension/src/options/App.vue
@@ -4,21 +4,14 @@
 </script>
 
 <template>
-  <div>
-    abcdef
+  <div class="title">
+    Coming soon ...
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.title {
+  padding: 100px 0;
+  text-align: center;
 }
 </style>
