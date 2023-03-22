6e59a76e71050f5f87f3d659595c8243d8b1a6dd
幻想 <huanxiangspace@qq.com>
2023-03-22 21:43:28 +0800

    config vue

--- blockchain/vue/vite.config.ts
+++ blockchain/vue/vite.config.ts
@@ -5,6 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 5678,
+    host: "127.0.0.1",
+  },
   plugins: [nodeResolve(), vue()],
   resolve: {
     alias: {
