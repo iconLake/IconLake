dd9490ad2daee307c50399a3a406762f3e5bcc74
HuanXiang <huanxiangspace@qq.com>
2023-04-25 22:31:52 +0800

    parse data

--- blockchain/ts-client/iconlake.icon/types.ts
+++ blockchain/ts-client/iconlake.icon/types.ts
@@ -1,7 +1,11 @@
 import { Params } from "./types/iconlake/icon/params"
+import { ClassData } from "./types/iconlake/icon/tx"
+import { IconData } from "./types/iconlake/icon/tx"
 
 
 export {     
     Params,
+    ClassData,
+    IconData,
     
  }
--- blockchain/x/icon/types/types.go
+++ blockchain/x/icon/types/types.go
@@ -1,1 +1,13 @@
 package types
+
+type IconDataI interface {
+	GetAuthor() string
+	GetName() string
+	GetDescription() string
+	GetCreateTime() string
+}
+
+type ClassDataI interface {
+	GetAuthor() string
+	GetCreateTime() string
+}
