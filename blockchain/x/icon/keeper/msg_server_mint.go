fcf6958194b7d4104e3f2985ecc06398a2dee740
HuanXiang <huanxiangspace@qq.com>
2023-04-26 23:51:51 +0800

    mint icon

--- blockchain/x/icon/keeper/msg_server_mint.go
+++ blockchain/x/icon/keeper/msg_server_mint.go
@@ -6,7 +6,10 @@ import (
 
 	"iconlake/x/icon/types"
 
-	codec "github.com/cosmos/cosmos-sdk/codec/types"
+	"strconv"
+
+	"cosmossdk.io/errors"
+	codecTypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/nft"
 )
@@ -20,11 +23,15 @@ func (k msgServer) Mint(goCtx context.Context, msg *types.MsgMint) (*types.MsgMi
 		return nil, err
 	}
 
-	if !k.nftKeeper.HasClass(ctx, msg.ClassId) {
+	msg.Data.Author = msg.Creator
+
+	classInfo, hasClass := k.nftKeeper.GetClass(ctx, msg.ClassId)
+	if !hasClass {
 		data := types.ClassData{
+			Author:     msg.Creator,
 			CreateTime: time.Now().Format(time.RFC3339),
 		}
-		dataAny, err := codec.NewAnyWithValue(&data)
+		dataAny, err := codecTypes.NewAnyWithValue(&data)
 		if err != nil {
 			k.Logger(ctx).Error(err.Error())
 			return nil, err
@@ -37,23 +44,51 @@ func (k msgServer) Mint(goCtx context.Context, msg *types.MsgMint) (*types.MsgMi
 			k.Logger(ctx).Error(err.Error())
 			return nil, err
 		}
+	} else {
+		var classData types.ClassDataI
+		err = k.cdc.UnpackAny(classInfo.Data, &classData)
+		if err != nil {
+			k.Logger(ctx).Error(err.Error())
+			return nil, err
+		}
+		if classData.GetAuthor() != msg.Creator {
+			err = errors.Wrapf(types.ErrPermissionDenay, "expected author is (%s)", classData.GetAuthor())
+			k.Logger(ctx).Error(err.Error())
+			return nil, err
+		}
 	}
 
-	dataAny, err := codec.NewAnyWithValue(msg.Data)
+	dataAny, err := codecTypes.NewAnyWithValue(msg.Data)
 	if err != nil {
 		k.Logger(ctx).Error(err.Error())
 		return nil, err
 	}
-	err = k.nftKeeper.Mint(ctx, nft.NFT{
-		ClassId: msg.ClassId,
-		Id:      msg.Id,
-		Uri:     msg.Uri,
-		UriHash: msg.UriHash,
-		Data:    dataAny,
-	}, accAddress)
-	if err != nil {
-		k.Logger(ctx).Error(err.Error())
-		return nil, err
+	if msg.Supply > 1 {
+		for i := 0; i < int(msg.Supply); i++ {
+			err = k.nftKeeper.Mint(ctx, nft.NFT{
+				ClassId: msg.ClassId,
+				Id:      msg.Id + ":" + strconv.Itoa(i),
+				Uri:     msg.Uri,
+				UriHash: msg.UriHash,
+				Data:    dataAny,
+			}, accAddress)
+			if err != nil {
+				k.Logger(ctx).Error(err.Error())
+				return nil, err
+			}
+		}
+	} else {
+		err = k.nftKeeper.Mint(ctx, nft.NFT{
+			ClassId: msg.ClassId,
+			Id:      msg.Id,
+			Uri:     msg.Uri,
+			UriHash: msg.UriHash,
+			Data:    dataAny,
+		}, accAddress)
+		if err != nil {
+			k.Logger(ctx).Error(err.Error())
+			return nil, err
+		}
 	}
 
 	_ = ctx
