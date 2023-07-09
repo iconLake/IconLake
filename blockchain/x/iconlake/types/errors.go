42b8f237328f30cfe2629ca70fa22899d80b0202
HuanXiang <huanxiangspace@qq.com>
2023-07-09 15:25:31 +0800

    mint drop

--- blockchain/x/iconlake/types/errors.go
+++ blockchain/x/iconlake/types/errors.go
@@ -3,10 +3,12 @@ package types
 // DONTCOVER
 
 import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	sdkerrors "cosmossdk.io/errors"
 )
 
 // x/iconlake module sentinel errors
 var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrAddress = sdkerrors.Register(ModuleName, 1100, "address error")
+	ErrDenom   = sdkerrors.Register(ModuleName, 1101, "denom error")
+	ErrAmount  = sdkerrors.Register(ModuleName, 1102, "amount error")
 )
