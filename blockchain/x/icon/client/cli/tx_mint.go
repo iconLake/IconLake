fc807601c30b711a2c5253e6916c476228d8ad43
HuanXiang <huanxiangspace@qq.com>
2023-07-11 23:52:07 +0800

    fix cmd of mint coin

--- blockchain/x/icon/client/cli/tx_mint.go
+++ blockchain/x/icon/client/cli/tx_mint.go
@@ -2,34 +2,43 @@ package cli
 
 import (
 	"strconv"
+	"time"
 
-	"encoding/json"
+	"iconlake/x/icon/types"
+
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
-	"iconlake/x/icon/types"
 )
 
 var _ = strconv.Itoa(0)
 
 func CmdMint() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "mint [class-id] [id] [uri] [uri-hash] [data] [supply]",
+		Use:   "mint [class-id] [id] [uri] [uri-hash] [data.author] [data.name] [data.description] [data.create-time] [supply]",
 		Short: "Broadcast message mint",
-		Args:  cobra.ExactArgs(6),
+		Args:  cobra.ExactArgs(9),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 			argClassId := args[0]
 			argId := args[1]
 			argUri := args[2]
 			argUriHash := args[3]
-			argData := new(types.IconData)
-			err = json.Unmarshal([]byte(args[4]), argData)
 			if err != nil {
 				return err
 			}
-			argSupply, err := cast.ToUint64E(args[5])
+			_, err = time.Parse(time.RFC3339, args[7])
+			if err != nil {
+				return err
+			}
+			argData := &types.IconData{
+				Author:      args[4],
+				Name:        args[5],
+				Description: args[6],
+				CreateTime:  args[7],
+			}
+			argSupply, err := cast.ToUint64E(args[8])
 			if err != nil {
 				return err
 			}
