f10c647638398a664cd8aa71863bcead49121c0c
HuanXiang <huanxiangspace@qq.com>
2023-09-03 22:54:05 +0800

    template

--- service/assets/exhibition/default-template.ts
+++ service/assets/exhibition/default-template.ts
@@ -18,54 +18,63 @@ interface Nft {
 }
 
 export default class DefaultTemplate extends HTMLElement {
+  static get observedAttributes () {
+    return ['info', 'nfts', 'pagination']
+  }
+
   constructor () {
     super()
 
-    const root = this.attachShadow({
+    this.attachShadow({
       mode: 'open'
     })
 
     const css = document.createElement('link')
     css.setAttribute('rel', 'stylesheet')
     css.setAttribute('href', './default-template.css')
-    root.appendChild(css)
+    this.shadowRoot?.appendChild(css)
 
-    const infoAttr = this.getAttribute('info')
-    if (infoAttr) {
-      try {
-        const info = JSON.parse(infoAttr)
+    let dom = document.createElement('div')
+    dom.className = 'info'
+    this.shadowRoot?.appendChild(dom)
+    dom = document.createElement('div')
+    dom.className = 'nfts'
+    this.shadowRoot?.appendChild(dom)
+  }
 
-        this.renderInfo(root, info)
-      } catch (err) {
-        console.error(err)
-      }
+  attributeChangedCallback (name: string, _oldValue, newValue: string | null) {
+    console.log(name, _oldValue, newValue)
+    if (!this.shadowRoot) {
+      return
     }
-
-    const nftsAttr = this.getAttribute('nfts')
-    if (nftsAttr) {
-      try {
-        const nfts = JSON.parse(nftsAttr)
-        this.renderNfts(root, nfts)
-      } catch (e) {
-        console.error(e)
-      }
+    switch (name) {
+      case 'info':
+        this.renderInfo(this.shadowRoot, newValue ? JSON.parse(newValue) : null)
+        break
+      case 'nfts':
+        this.renderNfts(this.shadowRoot, newValue ? JSON.parse(newValue) : null)
+        break
     }
   }
 
-  renderInfo (root: ShadowRoot, info: Info) {
-    const infoDom = document.createElement('div')
-    infoDom.className = 'info'
+  renderInfo (root: ShadowRoot, info: Info | null) {
+    const infoDom = this.shadowRoot?.querySelector('.info')
+    if (!infoDom) {
+      return
+    }
+
+    infoDom.innerHTML = ''
     const coverDom = document.createElement('div')
-    coverDom.style.backgroundImage = `url(${info.uri})`
+    coverDom.style.backgroundImage = `url(${info?.uri})`
     coverDom.className = 'cover'
     infoDom.appendChild(coverDom)
     const contentDom = document.createElement('div')
     contentDom.className = 'content'
     coverDom.appendChild(contentDom)
     const nameDom = document.createElement('h1')
-    nameDom.innerText = info.name
+    nameDom.innerText = info?.name ?? ''
     contentDom.appendChild(nameDom)
-    if (info.description) {
+    if (info?.description) {
       const descDom = document.createElement('h2')
       descDom.innerText = info.description
       contentDom.appendChild(descDom)
@@ -75,9 +84,33 @@ export default class DefaultTemplate extends HTMLElement {
   }
 
   renderNfts (root, nfts: Nft[]) {
-    const nftsDom = document.createElement('div')
-    nftsDom.className = 'nfts'
+    const nftsDom = this.shadowRoot?.querySelector('.nfts')
+    if (!nftsDom) {
+      return
+    }
+
+    nftsDom.innerHTML = ''
+    const containerDom = document.createElement('div')
+    containerDom.className = 'nfts-container'
+    nftsDom.appendChild(containerDom)
+    nfts.forEach(e => {
+      this.renderNft(containerDom, e)
+    })
 
     root.appendChild(nftsDom)
+  }
+
+  renderNft (root, nft: Nft) {
+    const nftDom = document.createElement('div')
+    nftDom.className = 'nft'
+    const imgDom = document.createElement('div')
+    imgDom.className = 'nft-img'
+    imgDom.style.backgroundImage = `url(${nft.uri})`
+    nftDom.appendChild(imgDom)
+    const nameDom = document.createElement('div')
+    nameDom.innerText = nft.data.name
+    nameDom.className = 'nft-name'
+    nftDom.appendChild(nameDom)
+    root.appendChild(nftDom)
   }
 }
--- service/assets/exhibition/project.ts
+++ service/assets/exhibition/project.ts
@@ -6,15 +6,16 @@
     return
   }
 
-  const info = await fetch(`${lcd}/cosmos/nft/v1beta1/classes/${projectId}`).then(e => e.json())
-
   const iconlakeDom = document.querySelector('iconlake-exhibition')
-  iconlakeDom?.setAttribute('info', JSON.stringify(info.class))
+
+  fetch(`${lcd}/cosmos/nft/v1beta1/classes/${projectId}`).then(e => e.json()).then(info => {
+    iconlakeDom?.setAttribute('info', JSON.stringify(info.class))
+  }).catch(console.error)
+
   import('./default-template.js').then(module => {
     customElements.define('iconlake-exhibition', module.default)
   })
 
-  // nfts
   fetch(`${lcd}/cosmos/nft/v1beta1/nfts?class_id=${projectId}`).then(e => e.json()).then(data => {
     iconlakeDom?.setAttribute('nfts', JSON.stringify(data.nfts))
     iconlakeDom?.setAttribute('pagination', JSON.stringify(data.pagination))
