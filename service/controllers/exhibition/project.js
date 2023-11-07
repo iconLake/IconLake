e4ce4fe749f4947f9746f02f56270cefcdc6963d
HuanXiang <huanxiangspace@qq.com>
2023-11-08 00:12:21 +0800

    render project for bot

--- service/controllers/exhibition/project.js
+++ service/controllers/exhibition/project.js
@@ -1,6 +1,11 @@
 import { RESOURCE_MAX_AGE, ROOT } from '../../utils/const.js'
+import isbot from 'isbot'
+import { renderProject } from './bot.js'
 
 export async function info (req, res) {
+  if (isbot(req.get('user-agent')) || req.query.bot) {
+    return renderProject(req, res)
+  }
   res.sendFile('./public/exhibition/project.html', {
     root: ROOT,
     maxAge: RESOURCE_MAX_AGE
