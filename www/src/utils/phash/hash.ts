b336297a5fc31a5ddd049fff80f37779f2cf7315
HuanXiang <huanxiangspace@qq.com>
2025-01-20 23:19:18 +0800

    feat: phash

---
+++ www/src/utils/phash/hash.ts
@@ -0,0 +1,189 @@
+export class ImageHash {
+
+  binArray;
+
+  constructor(binArray: Uint8ClampedArray<ArrayBuffer>) {
+    this.binArray = binArray;
+  }
+
+  static fromBase64(s: string) {
+    const buf = atob(s);
+    const arr = new Uint8ClampedArray(buf.length * 8);
+
+    for (let i = 0; i < buf.length; i++) {
+      const c = buf.charCodeAt(i);
+      arr[i * 8] = (c & 0x01);
+      arr[i * 8 + 1] = (c & 0x02) >> 1;
+      arr[i * 8 + 2] = (c & 0x04) >> 2;
+      arr[i * 8 + 3] = (c & 0x08) >> 3;
+      arr[i * 8 + 4] = (c & 0x10) >> 4;
+      arr[i * 8 + 5] = (c & 0x20) >> 5;
+      arr[i * 8 + 6] = (c & 0x40) >> 6;
+      arr[i * 8 + 7] = (c & 0x80) >> 7;
+    }
+
+    return new ImageHash(arr);
+  }
+
+  static fromHexStringReversed(s: string) {
+    if (s.length % 2 !== 0) {
+      throw Error("hex string length must be a multiple of 2");
+    }
+    const arr = new Uint8ClampedArray(s.length * 4);
+
+    for (let i = 0; i < s.length; i += 2) {
+      const c = Number.parseInt(s.slice(i, i + 2), 16);
+
+      if (Number.isNaN(c)) {
+        throw Error("Invalid hex string");
+      }
+
+      arr[i * 4] = (c & 0x80) >> 7;
+      arr[i * 4 + 1] = (c & 0x40) >> 6;
+      arr[i * 4 + 2] = (c & 0x20) >> 5;
+      arr[i * 4 + 3] = (c & 0x10) >> 4;
+      arr[i * 4 + 4] = (c & 0x08) >> 3;
+      arr[i * 4 + 5] = (c & 0x04) >> 2;
+      arr[i * 4 + 6] = (c & 0x02) >> 1;
+      arr[i * 4 + 7] = (c & 0x01);
+    }
+
+    return new ImageHash(arr);
+  }
+
+  static fromHexString(s: string) {
+    if (s.length % 2 !== 0) {
+      throw Error("hex string length must be a multiple of 2");
+    }
+    const arr = new Uint8ClampedArray(s.length * 4);
+
+    for (let i = 0; i < s.length; i += 2) {
+      const c = Number.parseInt(s.slice(i, i + 2), 16);
+
+      if (Number.isNaN(c)) {
+        throw Error("Invalid hex string");
+      }
+
+      arr[i * 4] = (c & 0x01);
+      arr[i * 4 + 1] = (c & 0x02) >> 1;
+      arr[i * 4 + 2] = (c & 0x04) >> 2;
+      arr[i * 4 + 3] = (c & 0x08) >> 3;
+      arr[i * 4 + 4] = (c & 0x10) >> 4;
+      arr[i * 4 + 5] = (c & 0x20) >> 5;
+      arr[i * 4 + 6] = (c & 0x40) >> 6;
+      arr[i * 4 + 7] = (c & 0x80) >> 7;
+    }
+
+    return new ImageHash(arr);
+  }
+
+  toHexStringReversed() {
+    let str = "";
+
+    for (let i = 0; i < this.binArray.length; i += 8) {
+      const c =
+        this.binArray[i] << 7 |
+        this.binArray[i + 1] << 6 |
+        this.binArray[i + 2] << 5 |
+        this.binArray[i + 3] << 4 |
+        this.binArray[i + 4] << 3 |
+        this.binArray[i + 5] << 2 |
+        this.binArray[i + 6] << 1 |
+        this.binArray[i + 7];
+
+      str += c.toString(16).padStart(2, "0");
+    }
+
+    return str;
+  }
+
+  toHexString() {
+    let str = "";
+
+    for (let i = 0; i < this.binArray.length; i += 8) {
+      const c =
+        this.binArray[i] |
+        this.binArray[i + 1] << 1 |
+        this.binArray[i + 2] << 2 |
+        this.binArray[i + 3] << 3 |
+        this.binArray[i + 4] << 4 |
+        this.binArray[i + 5] << 5 |
+        this.binArray[i + 6] << 6 |
+        this.binArray[i + 7] << 7;
+
+      str += c.toString(16).padStart(2, "0");
+    }
+
+    return str;
+  }
+
+  toBase64() {
+    let buf = [];
+
+    for (let i = 0; i < this.binArray.length; i += 8) {
+      buf.push(this.binArray[i] |
+        this.binArray[i + 1] << 1 |
+        this.binArray[i + 2] << 2 |
+        this.binArray[i + 3] << 3 |
+        this.binArray[i + 4] << 4 |
+        this.binArray[i + 5] << 5 |
+        this.binArray[i + 6] << 6 |
+        this.binArray[i + 7] << 7)
+    }
+
+    return btoa(String.fromCharCode(...new Uint8Array(buf)));
+  }
+
+  hammingDistance(hash: ImageHash) {
+    if (hash.binArray.length !== this.binArray.length) {
+      throw new Error("Cannot compare two ImageHash instances of different sizes");
+    }
+
+    let distance = 0;
+    for (let i = 0; i < this.binArray.length; i++) {
+      if (this.binArray[i] !== hash.binArray[i]) {
+        distance += 1;
+      }
+    }
+
+    return distance;
+  }
+}
+
+export class ImageMultiHash {
+  segmentHashes: ImageHash[]
+
+  constructor(hashes: ImageHash[]) {
+    this.segmentHashes = hashes;
+  }
+
+  toJSON() {
+    return this.segmentHashes.map((h: { toHexString: () => any; }) => h.toHexString());
+  }
+
+  static fromJSON(json: any[]) {
+    return new ImageMultiHash(json.map((s: any) => ImageHash.fromHexString(s)));
+  }
+
+  hashDiff(hash: ImageMultiHash, hammingCutoff: number) {
+    let sum = 0;
+    let num = 0;
+
+    for (let i = 0; i < hash.segmentHashes.length; i++) {
+
+      const distances = [];
+
+      for (let j = 0; j < this.segmentHashes.length; j++) {
+        distances.push(hash.segmentHashes[i].hammingDistance(this.segmentHashes[j]));
+      }
+
+      const minDistance = Math.min(...distances);
+      if (minDistance <= hammingCutoff) {
+        sum += minDistance;
+        num += 1;
+      }
+    }
+
+    return { num, sum };
+  }
+}
---
+++ www/src/utils/phash/img.ts
@@ -0,0 +1,41 @@
+class CanvasUtil {
+  async resizeImageAndGetData(image: HTMLImageElement, width: number, height: number) {
+    let canvas = await this.resizeImage(image, width, height);
+
+    const ctx = canvas.getContext("2d");
+    return ctx!.getImageData(0, 0, width, height).data;
+  }
+
+  async resizeImage(image: HTMLImageElement, width: number, height: number) {
+    let toCanvas = document.createElement("canvas");
+    toCanvas.width = width;
+    toCanvas.height = height;
+
+    const toCtx = toCanvas.getContext("2d");
+    toCtx!.drawImage(image, 0, 0, width, height);
+
+    return toCanvas;
+  }
+}
+
+export const canvasUtil = new CanvasUtil();
+
+class GrayScaleConverter {
+  /**
+   *
+   * RGBA -> L (ITU-R 601-2 luma transform)
+   *
+   * @param imgData {Uint8ClampedArray}
+   */
+  convert(imgData: Uint8ClampedArray) {
+    const arr = new Uint8ClampedArray(imgData.length / 4);
+
+    for (let i = 0; i < imgData.length; i += 4) {
+      arr[i >> 2] = Math.round((imgData[i] * 299 / 1000 + imgData[i + 1] * 587 / 1000 + imgData[i + 2] * 114 / 1000))
+    }
+
+    return arr;
+  }
+}
+
+export const grayScaleConverter = new GrayScaleConverter()
