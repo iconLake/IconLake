9459b3d081dab0533c8e4e89a64db8d1536f0dbf
HuanXiang <huanxiangspace@qq.com>
2025-04-18 22:05:37 +0800

    fix: env

--- mcp/ecosystem.config.cjs
+++ mcp/ecosystem.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
     watch: 'dist',
     log_date_format: 'YYYY-MM-DD HH:mm Z',
     env: {
+      ICONLAKE_ENDPOINT: 'https://iconlake.com',
       ICONLAKE_ACCESS_KEY: 'your-access-key-here',
     }
   }]
