c9391d2d9dc6922afc9f2274db3c47dbe86652d1
HuanXiang <huanxiangspace@qq.com>
2025-06-12 14:59:00 +0800

    fix: search options

--- www/src/views/icons/search/Web.vue
+++ www/src/views/icons/search/Web.vue
@@ -23,7 +23,7 @@
           </div>
         </div>
         <div
-          v-if="options.length > 0 && !keywords"
+          v-if="options.length > 0"
           class="options flex end"
         >
           <template
