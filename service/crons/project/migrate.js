382278006deb70025f977ae73b3f7735095c379f
HuanXiang <huanxiangspace@qq.com>
2025-07-21 15:57:29 +0800

    fix: data

--- service/crons/project/migrate.js
+++ service/crons/project/migrate.js
@@ -1,26 +1,18 @@
-import crypto from 'crypto'
-import { save } from '../../utils/file.js'
-import { hasObject } from '../../utils/cos.js'
-
 export async function migrate (project) {
   const len = project.icons.length
   let updateNum = 0
   for (let i = 0; i < len; i++) {
     const icon = project.icons[i]
-    if (!icon.svg || !icon.svg.path) {
+    if (!icon.txHash) {
       continue
     }
-    const content = `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="${icon.svg.viewBox}">${icon.svg.path}</svg>`
-    const hash = crypto.createHash('md5').update(content).digest('hex')
-    const fileKey = `icon/${project._id}/${hash}.svg`
-    const isExist = await hasObject(fileKey)
-    if (isExist) {
-      icon.svg.path = undefined
-      icon.svg.viewBox = undefined
-    } else {
-      const data = await save(`${hash}.svg`, content, `icon/${project._id}/`)
-      icon.svg.url = data.key
+    icon.blockchain = {
+      txHash: icon.txHash,
+      height: 0,
+      classId: project._id,
+      nftId: icon._id
     }
+    icon.txHash = undefined
     updateNum++
   }
   if (updateNum === 0) {
