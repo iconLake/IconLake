e60cbb84f51f172fa4b8b860adc03f4c0fb696f7
HuanXiang <huanxiangspace@qq.com>
2025-07-30 23:50:51 +0800

    fix: extension upload files to custom

--- extension/src/apis/project.ts
+++ extension/src/apis/project.ts
@@ -17,13 +17,59 @@ export function addIcon(projectId: string, icons: IconResource[]) {
   })
 }
 
-export function uploadFile(projectId: string, _id: string, data: string | Blob, dir?: string): Promise<void | {
+async function uploadFileToCustom(params: {
+  projectId: string
+  _id: string
+  data: string | ArrayBuffer | Blob
+  dir?: string
+}, config: {
+  api: string
+  token: string
+}): Promise<void | {
+  key: string
+  url: string
+}> {
+  const data = new FormData()
+  let file = params.data
+  if (file instanceof Blob) {
+    file = new File([await file.arrayBuffer()], params._id, { type: file.type })
+  } else if (file instanceof ArrayBuffer) {
+    file = new File([file], params._id, { type: 'application/octet-stream' })
+  } else {
+    file = new File([params.data], params._id, { type: 'image/svg+xml' })
+  }
+  data.append('file', file)
+  data.append('key', `${params.dir || 'icon'}/${params.projectId}/${params._id}`)
+  const headers = new Headers()
+  headers.append('Authorization', `Bearer ${config.token}`)
+  return await request(config.api, {
+    method: 'POST',
+    headers,
+    body: data
+  })
+}
+
+export async function uploadFile(projectId: string, _id: string, data: string | Blob, dir?: string): Promise<void | {
   url: string;
   key: string;
 }> {
+  const info = await request<{
+    custom?: {
+      api: string
+      token: string
+    }
+  }>(`${baseUrl}/file/storageInfo?projectId=${projectId}`)
+  if (info?.custom && info.custom.api) {
+    return await uploadFileToCustom({
+      projectId,
+      _id,
+      data,
+      dir
+    }, info.custom)
+  }
   const headers = new Headers()
   headers.append('Content-Type', typeof data === 'string' ? 'text/plain; charset=utf-8' : 'application/octet-stream')
-  return request(`${baseUrl}/file/upload?projectId=${projectId}&_id=${_id}&dir=${dir}`, {
+  return await request(`${baseUrl}/file/upload?projectId=${projectId}&_id=${_id}&dir=${dir}`, {
     method: 'POST',
     headers,
     body: data,
